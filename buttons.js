64c449d36becb3fe70ff696d13443e819c7d993d
cynthion <ice100k@outlook.com>
2025-03-16 10:47:18 +0000

    forgor bedman

--- buttons.js
+++ buttons.js
@@ -63,7 +63,7 @@ async function downloadImage(imageSrc, imgname) {
 // Load Content
 
   const avatars = ["cat", "charlotte", "god", "icon", "beetd", "slug", "godofeverythingelse"]
-  const artwork = ["catbg", "Charlotte", "crazy", "cynthBanner", "preview", "timelapse", "neglectsShowcase", "pawn", "sunBeam", "beeTDirl", "terrariaEOC", "stormboundhalloween", "plasmaClocket", "lurantis", "charlottesLegacy-Title", "wanderingVillage", "jellyfish"]
+  const artwork = ["catbg", "Charlotte", "crazy", "cynthBanner", "preview", "timelapse", "neglectsShowcase", "pawn", "sunBeam", "beeTDirl", "terrariaEOC", "stormboundhalloween", "plasmaClocket", "lurantis", "charlottesLegacy-Title", "wanderingVillage", "jellyfish", "bedman"]
   const characters = ["ErraticNegelectsEmpty", "sylveon", "charlotte", "cynthion", "hollowKnight", "ghostling", "Bearz", "strawberryCapKittens", "strawberryNeglects", "cynton", "seaslug", "perspective", "cardconveyorplayer", "BeeTd"]
 
   const main = document.getElementById("main");
